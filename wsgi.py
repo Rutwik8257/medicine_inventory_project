a96b0192665517917a5ef163542cecf9e298a03c
Rutwik8257 <139038132+Rutwik8257@users.noreply.github.com>
2024-11-27 17:41:36 +0530

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+import os
+from app import app
+
+if _name_ == "_main_":
+    port = int(os.environ.get("PORT", 4000))  # Get the port from environment or use 5000 for local
+    app.run(host="0.0.0.0", port=port)
